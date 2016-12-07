ce180d584403b65e05392c3a3377e0cffe77eda3
Finnur Stefansson <Finnur Stefansson>
2016-12-07 20:26:47 +0000

    Added GETTERS and SETTERS to the class

--- HonaristaLogic/src/entity/Owns.java
+++ HonaristaLogic/src/entity/Owns.java
@@ -1,6 +1,16 @@
 package entity;
 
 public class Owns {
+	//ATTRIBUTES
 	private User _vendor;
 	private Shop _shop;
+	
+	//TODO
+	
+	//GETTERS
+	public User getUser(){ return _vendor; }
+	public Shop getShop(){ return _shop; }
+	//SETTERS
+	public User setUser(User vendor){ _vendor = vendor; }
+	public Shop setShop(Shop shop){ _shop = shop; }
 }
