d17a9294a9a14f7974dc7920375709bb7a468f0e
Finnur Stefansson <Finnur Stefansson>
2016-12-07 20:19:21 +0000

    Set up SETTERS and GETTERS for the class

--- HonaristaLogic/src/entity/Tags.java
+++ HonaristaLogic/src/entity/Tags.java
@@ -1,11 +1,17 @@
 package entity;
 
 public class Tags {
-	private String _category;
-	Item item;
 	//ATTRIBUTES
+	private String _category;
+	private Item _item;
+	
 	//TODO
+	
 	//GETTERS
+	public String category(){ return _category; }
+	public Item getItem(){ return _item; }
 	//SETTERS
+	public setCategory(String category){ _category = category; }
+	public setItem(Item item){ _item = item; }
 
 }
