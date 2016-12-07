dc64a7332fc4e704b0befc4432d54deab62dabaa
Finnur Stefansson <Finnur Stefansson>
2016-12-07 20:17:59 +0000

    Set up GETTERS and SETTERS for the class

--- HonaristaLogic/src/entity/ShoppedAt.java
+++ HonaristaLogic/src/entity/ShoppedAt.java
@@ -5,8 +5,16 @@ public class ShoppedAt {
 	private Shop _shop;
 	private User _user;
 	private String _review;
+	
 	//TODO
+	
 	//GETTERS
+	public Shop getShop() {	return _shop; }
+	public User getUser() { return _user; }
+	public String getReview() {	return _review;	}
 	//SETTERS
+	public void setShop(Shop shop){ _shop = shop; }
+	public void setUser(User user){ _user = user; }
+	public void setReview(String review){ _review = review; }
 
 }
